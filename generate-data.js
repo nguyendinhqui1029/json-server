482da6baa3a6e0144386058c14fa585ce0f9f534
nguyenqui <Qui_Nguyen_Dinh@mfcgd.com>
2021-07-13 19:48:53 +0700

    deploy add

--- generate-data.js
+++ generate-data.js
@@ -101,18 +101,77 @@ generateUsers = (number) => {
       )
     );
   });
-  subCategoriesLevel2.forEach((categoryLevel2)=>{
-     categoryLevel2.id !== "null" &&
-       products.push(...generateProducts(50, categoryLevel2.id));
-     categoryLevel2.id !== "null" &&
-       news.push(...generateNews(10, categoryLevel2.id));
+  subCategoriesLevel2.forEach((categoryLevel2) => {
+    categoryLevel2.id !== "null" &&
+      products.push(...generateProducts(50, categoryLevel2.id));
+    categoryLevel2.id !== "null" &&
+      news.push(...generateNews(10, categoryLevel2.id));
   });
   const users = generateUsers(10);
   const db = {
     users: users,
-    categories: subCategoriesLevel1.concat(subCategoriesLevel2).concat(categories),
+    categories: subCategoriesLevel1
+      .concat(subCategoriesLevel2)
+      .concat(categories),
     news: news,
     products: products,
+    deliveryMethod: [
+      {
+        id: "5d1b8222-8241-45dd-ac89-70d1793b6f4b",
+        name: "Giao hàng nhanh",
+        fee: 10000,
+      },
+      {
+        id: "3d1b8222-8241-45dd-ac89-70d1793b6f4b",
+        name: "Gửi hàng qua bưu điện",
+        fee: 10000,
+      },
+    ],
+    paymentMethod: [
+      {
+        id: "3d1b8222-8241-45dd-ac89-70d1793b6f4s",
+        name: "Thanh toán khi giao hàng (COD)",
+        isShow: true,
+      },
+      {
+        id: "3d1b8222-8241-45dd-ac89-70d1793b6f4c",
+        name: "Thanh toán online qua cổng OnePay bằng thẻ ATM nội địa",
+        isShow: true,
+      },
+      {
+        id: "3d1b8222-8241-45dd-ac89-70d1793b6f4z",
+        name: "Chuyển khoản qua ngân hàng",
+        isShow: false,
+      },
+      {
+        id: "3d1b8222-8241-45dd-ac89-70d1793b6f4n",
+        name: "Thanh toán online qua cổng OnePay bằng thẻ Visa/Master/JCB",
+        isShow: false,
+      },
+    ],
+    orderCarts: [
+      {
+        id: "5d1b8222-8241-45dd-ac89-70d1793b6f4a",
+        idCustomer: "5d1b8222-8241-45dd-ac89-70d1793b6f4a",
+        dataList: {
+          id: "5d1b8222-8241-45dd-ac89-70d1793b6f4a",
+          productName: "Fantastic Metal Pants",
+          subContent:
+            "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-d",
+          detailContent:
+            "Ergonomic executive chair upholstered in bonded bl…ded seat and back for all-day comfort and support",
+          imageUrl: "http://placeimg.com/640/480/fashion",
+        },
+        address: {},
+        addressOther: {},
+        idDeliveryMethod: "",
+        idPaymentMethod: "",
+        deliveryFee: 0,
+        intoMoney: 736955.2000000001,
+        totalPay: 736955.2000000001,
+        status: "watting payment",
+      },
+    ],
     info: {},
   };
 
